ab9cd846177749ea280aa6a69dea6dc0bfa9f633
RafaelForbeck <rafaelforbeck@gmail.com>
2025-03-13 11:45:19 -0300

    treino

--- scripts/player.gd
+++ scripts/player.gd
@@ -16,7 +16,7 @@ func _physics_process(delta: float) -> void:
 			anim.play("idle")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump"): # and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		anim.play("jump")
 
