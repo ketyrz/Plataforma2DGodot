cac75de2fd237e0ac48940947cc29796ef9f609e
Rafael Forbeck <rafaelforbeck@gmail.com>
2025-04-10 21:22:23 -0300

    Adiciona inimigo

--- scripts/player.gd
+++ scripts/player.gd
@@ -22,9 +22,7 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and jump_count < max_jump_count:
-		velocity.y = JUMP_VELOCITY
-		jump_count += 1 
-		anim.play("jump")
+		jump()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -43,6 +41,11 @@ func _physics_process(delta: float) -> void:
 		
 	move_and_slide()
 
+func jump():
+	velocity.y = JUMP_VELOCITY
+	jump_count += 1 
+	anim.play("jump")
+
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("DeathZone"):
 		call_deferred("reload_scene")
@@ -52,6 +55,13 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 			call_deferred("load_scene", next_level)
 		else:
 			push_error("Próxima fase não definida em EndLevel")
+	elif area.is_in_group("Enemies"):
+		if velocity.y > 0: # o player matou o inimigo
+			area.take_damage() # deleta o inimigo
+			jump()
+		else:
+			reload_scene()
+			
 
 func reload_scene():
 	get_tree().reload_current_scene()
